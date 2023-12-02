82b09120f0c4a4a03dd47bca01f4905272c587ee
sylvi <20102981@mail.wit.ie>
2023-12-02 16:02:36 +0000

    Ensure use of DRY code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,23 @@
 //1. Import Statements
+/**
+ * 1. Imports clientApi class from controllers package
+ * 2. Imports client data model from models package
+ * 3. Imports scanner input class from utils package
+ * 4. Imports readNextInt method from scanner input class in utils package
+ * 5. Imports readNextLine method from scanner input class in utils package
+ * 6. Imports function to exit the system
+ */
 import controllers.ClientAPI
 import models.Client
 import utils.ScannerInput
+import utils.ScannerInput.readNextInt
+import utils.ScannerInput.readNextLine
+import kotlin.system.exitProcess
+
 
 //2. Variables
 private val clientAPI = ClientAPI()
+
 
 //3. Methods
 /**
@@ -13,20 +26,14 @@ private val clientAPI = ClientAPI()
  * Where the program starts running
  * Calls runMenu()
  */
-fun main()
-{
-    runMenu()
-
-}
+fun main() = runMenu()
 
 /**
  * 2. mainMenu()
  * Displays menu
  * Allows user input
  */
-fun mainMenu() : Int
-{
-    return ScannerInput.readNextInt(""" 
+fun mainMenu() = readNextInt(""" 
         > ----------------------------------
         > |     BEAUTY SALON APP
         > |----------------------------------
@@ -36,35 +43,38 @@ fun mainMenu() : Int
         > |     APPOINTMENT MENU
         > | 1)  Add an appointment
         > |----------------------------------
+        > | 0) Exit App
         > ==>> """.trimMargin(">"))
-}
-          
-
 
 /**
  * 3. runMenu()
  * Loops mainMenu()
  * Processes user's choice
  */
-fun runMenu()
-{
-
-
+fun runMenu() {
+    do {
+        when (val option = mainMenu()) {
+            1 -> addClient()
+            0 -> exitApp()
+            else -> println("Invalid menu choice: $option")
+        }
+    } while (true)
 }
+
 
 /**
  * 4. addClient()
  * Method to add a note
  **/
 fun addClient() {
-    val clientId = ScannerInput.readNextInt("Enter the client's Id: ")
-    val firstName = ScannerInput.readNextLine("Enter the client's first name: ")
-    val lastName = ScannerInput.readNextLine("Enter the client's last name: ")
-    val street = ScannerInput.readNextLine("Enter the client's street: ")
-    val county = ScannerInput.readNextLine("Enter the client's county: ")
-    val email = ScannerInput.readNextLine("Enter the client's email address: ")
-    val phone = ScannerInput.readNextInt("Enter the client's phone number: ")
-    val allergy = ScannerInput.readNextLine("Enter the client's allergies: ")
+    val clientId = readNextInt("Enter the client's Id: ")
+    val firstName = readNextLine("Enter the client's first name: ")
+    val lastName = readNextLine("Enter the client's last name: ")
+    val street = readNextLine("Enter the client's street: ")
+    val county = readNextLine("Enter the client's county: ")
+    val email = readNextLine("Enter the client's email address: ")
+    val phone = readNextInt("Enter the client's phone number: ")
+    val allergy = readNextLine("Enter the client's allergies: ")
     val isAdded = clientAPI.addClient(Client(clientId = clientId, firstName = firstName, lastName = lastName, street = street, county = county, email = email, phone = phone, allergy = allergy))
     if (isAdded) {
         println("Client $clientId Added Successfully")
@@ -73,3 +83,12 @@ fun addClient() {
     }
 }
 
+
+/**
+ * exitApp()
+ * Function to exit the app
+ */
+fun exitApp() {
+    println("Exiting...bye")
+    exitProcess(0)
+}
