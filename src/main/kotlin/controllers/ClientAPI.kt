87530bfb16dd82b961c88e394e73793f2bfd180f
sylvi <20102981@mail.wit.ie>
2023-12-02 12:33:33 +0000

    Add skeleton of methods to handle menu

---
+++ src/main/kotlin/controllers/ClientAPI.kt
@@ -0,0 +1,21 @@
+//1. Packages
+/**
+ * Groups related classes which process user input
+ */
+package controllers
+
+import models.Client
+import java.util.ArrayList
+
+
+class ClientAPI
+{
+    //2. Variables
+    /**
+     * ArrayList to store list of client objects
+     */
+    private var clients = ArrayList<Client>()
+
+    //3. Functions to manage client arrayList
+
+ }
