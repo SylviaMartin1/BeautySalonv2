41992d42fe83694dafe205d72be1cec0521c5330
sylvi <20102981@mail.wit.ie>
2023-12-02 09:20:53 +0000

    Create client data model

---
+++ src/main/kotlin/models/Client.kt
@@ -0,0 +1,21 @@
+//1. Packages
+/**
+ * Groups related data structures
+ */
+package models
+
+//2. Classes
+/**
+ * Groups all client data models in the application
+ */
+data class Client(
+    var firstName: String,
+    var lastName: String,
+    var street: String,
+    var county: String,
+    var email: String,
+    var phone: Int,
+    var allergy: String // -- list of Strings
+)
+
+
